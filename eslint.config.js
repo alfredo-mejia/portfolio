a9c54597b17039ab5f62113b465b7116ad61859b
Alfredo Mejia <49002795+alfredo-mejia@users.noreply.github.com>
2026-01-23 00:30:43 -0600

    setup the project

---
+++ eslint.config.js
@@ -0,0 +1,45 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import reactDom from 'eslint-plugin-react-dom'
+import reactX from 'eslint-plugin-react-x'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      // tseslint.configs.recommended,
+      // tseslint.configs.recommendedTypeChecked,
+      tseslint.configs.strictTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.strictTypeChecked,
+      reactRefresh.configs.stylisticTypeChecked,
+      reactRefresh.configs.vite,
+      reactX.configs.strictTypeChecked,
+      reactX.configs.stylisticTypeChecked,
+      reactX.configs.vite,
+      reactDom.configs.strictTypeChecked,
+      reactDom.configs.stylisticTypeChecked,
+      reactDom.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "no-console": "warn",
+      "prefer-const": "error",
+    },
+  },
+])
+
